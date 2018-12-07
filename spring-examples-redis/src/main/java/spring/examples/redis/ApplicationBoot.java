9c2308ff95b8a8ce66bf519ac23127efa2042dd9
yingzhuo <yingzhor@gmail.com>
2018-12-07 14:34:17 +0800

    update

--- spring-examples-redis/src/main/java/spring/examples/redis/ApplicationBoot.java
+++ spring-examples-redis/src/main/java/spring/examples/redis/ApplicationBoot.java
@@ -1,14 +1,28 @@
 package spring.examples.redis;
 
+import lombok.val;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
 @SpringBootApplication
-public class ApplicationBoot {
+public class ApplicationBoot implements ApplicationRunner {
+
+    @Autowired
+    private StringRedisTemplate redisTemplate;
 
     public static void main(String[] args) {
         SpringApplication application = new SpringApplication(ApplicationBoot.class);
         application.run(args);
+    }
+
+    @Override
+    public void run(ApplicationArguments args) {
+        val v = redisTemplate.opsForValue().get("yingzhuo");
+        System.out.println(v);
     }
 
 }
