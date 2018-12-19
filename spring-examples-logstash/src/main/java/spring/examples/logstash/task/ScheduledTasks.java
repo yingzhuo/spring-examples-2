b2d61e695d445f22b7eeba8361cb50cbd647e7b7
yingzhuo <yingzhor@gmail.com>
2018-12-19 16:49:36 +0800

    update

--- spring-examples-logstash/src/main/java/spring/examples/logstash/task/ScheduledTasks.java
+++ spring-examples-logstash/src/main/java/spring/examples/logstash/task/ScheduledTasks.java
@@ -1,21 +1,24 @@
 package spring.examples.logstash.task;
 
-import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-@Slf4j
 @Component
 public class ScheduledTasks {
 
     private static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("HH:mm:ss");
+    private static final Logger LOGGER_A = LoggerFactory.getLogger("A");
+    private static final Logger LOGGER_B = LoggerFactory.getLogger("B");
 
-    @Scheduled(fixedRate = 2000)
+    @Scheduled(fixedRate = 3000)
     public void scheduleTaskWithFixedRate() {
-        log.info("Fixed Rate Task :: Execution Time - {}", DATE_FORMAT.format(LocalDateTime.now()));
+        LOGGER_A.info("Fixed Rate Task :: Execution Time - {}", DATE_FORMAT.format(LocalDateTime.now()));
+        LOGGER_B.info("Fixed Rate Task :: Execution Time - {}", DATE_FORMAT.format(LocalDateTime.now()));
     }
 
 }
