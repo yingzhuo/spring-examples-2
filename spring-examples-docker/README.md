b17d838547e6cf1f33aa74eed0bdfc0a0b6756ff
yingzhuo <yingzhor@gmail.com>
2019-06-04 14:42:03 +0800

    update

--- spring-examples-docker/README.md
+++ spring-examples-docker/README.md
@@ -1,1 +1,9 @@
 # spring-examples-docker
+
+
+```bash
+docker build \
+        --build-arg JAR_FILE=*.jar \
+        -t yingzhuo/xxxx:${TAG} \
+        /path/to/target/docker-context/
+```
--- spring-examples-docker/src/main/docker/docker-entrypoint.sh
+++ spring-examples-docker/src/main/docker/docker-entrypoint.sh
@@ -8,6 +8,7 @@ exec /usr/bin/java \
     -Djava.security.egd=file:/dev/./urandom \
     -Duser.timezone=${TZ:-Asia/Shanghai} \
     -Djava.io.tmpdir=/application/tmp \
+    -cp  /application/lib/* \
     -jar /application/app.jar \
     --spring.profiles.active=${SPRING_PROFILES_ACTIVE:-docker} \
     ${JAVA_OPTS} \
