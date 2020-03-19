604e757759567e4510f411d98f0ce5045ee05943
yingzhuo <yingzhor@gmail.com>
2020-03-19 10:46:14 +0800

    2020-03-19 10:46:11

--- spring-examples-validation/src/main/java/spring/examples/validation/ApplicationConfigValidation.java
+++ spring-examples-validation/src/main/java/spring/examples/validation/ApplicationConfigValidation.java
@@ -11,21 +11,25 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
+import java.util.Locale;
+
 @Configuration
 public class ApplicationConfigValidation implements WebMvcConfigurer {
 
     @Bean
     public MessageSource messageSource() {
         final ReloadableResourceBundleMessageSource bean = new ReloadableResourceBundleMessageSource();
-        bean.setBasename("classpath:i18n/messages");
+        bean.setBasenames("classpath:config/i18n/messages");
         bean.setDefaultEncoding("UTF-8");
+        bean.setDefaultLocale(Locale.CHINA);
+        bean.setCacheSeconds(600);
         return bean;
     }
 
     @Bean
-    public LocalValidatorFactoryBean getValidator() {
-        LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
-        bean.setValidationMessageSource(messageSource());
+    public LocalValidatorFactoryBean getValidator(MessageSource messageSource) {
+        final LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
+        bean.setValidationMessageSource(messageSource);
         return bean;
     }
 
