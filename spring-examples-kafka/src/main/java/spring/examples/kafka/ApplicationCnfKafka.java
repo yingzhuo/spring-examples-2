f844300d4ab647f4330da30075957eb70e3185c9
yingzhuo <yingzhor@gmail.com>
2020-04-01 13:12:16 +0800

    2020-04-01 13:12:14

--- spring-examples-kafka/src/main/java/spring/examples/kafka/ApplicationCnfKafka.java
+++ spring-examples-kafka/src/main/java/spring/examples/kafka/ApplicationCnfKafka.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.listener.ContainerProperties;
 
@@ -14,7 +15,10 @@ public class ApplicationCnfKafka {
 
     @Bean
     public NewTopic topic1() {
-        return new NewTopic("test_topic", 15, (short) 1);
+        return TopicBuilder.name("test_topic")
+                .partitions(3)
+                .replicas(3)
+                .build();
     }
 
     @Bean
--- spring-examples-kafka/src/main/java/spring/examples/kafka/consumer/ConsumerBean.java
+++ spring-examples-kafka/src/main/java/spring/examples/kafka/consumer/ConsumerBean.java
@@ -1,6 +1,7 @@
 package spring.examples.kafka.consumer;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.messaging.handler.annotation.Payload;
@@ -13,9 +14,10 @@ public class ConsumerBean {
     private static final String GROUP_ID = "spring-examples";
 
     @KafkaListener(topics = "test_topic", groupId = GROUP_ID, containerFactory = "kafkaManualAckListenerContainerFactory")
-    public void listen(@Payload String message, Acknowledgment ack) {
-        log.debug("received message in group {}: {}", GROUP_ID, message);
+    public void listen(@Payload ConsumerRecord<String, String> record, Acknowledgment ack) {
+        log.debug("received message in group {}: {}", GROUP_ID, record.value());
         ack.acknowledge();
+//        ack.nack(1000L);
     }
 
 }
