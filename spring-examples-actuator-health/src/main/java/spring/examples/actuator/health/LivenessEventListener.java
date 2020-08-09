fe197a26de3948c7267ec403693a1f1718168fef
yingzhuo <yingzhor@gmail.com>
2020-08-10 05:47:52 +0800

    2020-08-10 05:47:50

--- spring-examples-actuator-health/src/main/java/spring/examples/actuator/health/LivenessEventListener.java
+++ spring-examples-actuator-health/src/main/java/spring/examples/actuator/health/LivenessEventListener.java
@@ -3,7 +3,6 @@ package spring.examples.actuator.health;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.availability.AvailabilityChangeEvent;
 import org.springframework.boot.availability.LivenessState;
-import org.springframework.boot.availability.ReadinessState;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
