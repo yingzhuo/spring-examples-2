b9f38dd7c3eba08294944393c6222f12df186798
应卓 <yingzhor@gmail.com>
2021-05-25 14:00:08 +0800

    2021-05-25 14:00:07

--- spring-examples-elasticsearch/src/main/java/spring/examples/elasticsearch/dao/EmployeeSearchDao.java
+++
@@ -1,7 +0,0 @@
-package spring.examples.elasticsearch.dao;
-
-import org.springframework.data.elasticsearch.repository.ElasticsearchCrudRepository;
-import spring.examples.elasticsearch.document.Employee;
-
-public interface EmployeeSearchDao extends ElasticsearchCrudRepository<Employee, String> {
-}
