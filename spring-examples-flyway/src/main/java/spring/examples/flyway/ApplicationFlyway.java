fe0f0b3ad7780acb67c3ff57cf4246e0ca76e9c4
应卓 <yingzhor@gmail.com>
2021-05-25 14:06:51 +0800

    2021-05-25 14:06:49

---
+++ spring-examples-flyway/src/main/java/spring/examples/flyway/ApplicationFlyway.java
@@ -0,0 +1,18 @@
+package spring.examples.flyway;
+
+import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApplicationFlyway {
+
+    @Bean
+    public FlywayMigrationStrategy cleanMigrateStrategy() {
+        return flyway -> {
+            flyway.repair();
+            flyway.migrate();
+        };
+    }
+
+}
