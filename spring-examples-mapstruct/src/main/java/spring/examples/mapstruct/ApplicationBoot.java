fb6fb735aac9285dad5875e82f1333aa6aa3ae77
应卓 <yingzhor@gmail.com>
2021-07-15 13:39:21 +0800

    2021-07-15 13:39:19

--- spring-examples-mapstruct/src/main/java/spring/examples/mapstruct/ApplicationBoot.java
+++ spring-examples-mapstruct/src/main/java/spring/examples/mapstruct/ApplicationBoot.java
@@ -1,19 +1,20 @@
 package spring.examples.mapstruct;
 
 import lombok.val;
-import org.mapstruct.extensions.spring.converter.ConversionServiceAdapter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.convert.ConversionService;
+import spring.examples.mapstruct.domain.Car;
 import spring.examples.mapstruct.domain.CarVO;
 
 @SpringBootApplication
 public class ApplicationBoot implements ApplicationRunner {
 
     @Autowired
-    private ConversionServiceAdapter adapter;
+    private ConversionService conversionService;
 
     public static void main(String[] args) {
         SpringApplication.run(ApplicationBoot.class, args);
@@ -22,7 +23,7 @@ public class ApplicationBoot implements ApplicationRunner {
     @Override
     public void run(ApplicationArguments args) throws Exception {
         val vo = new CarVO("vm", 5, "SEDAN");
-        System.out.println(adapter.mapCarVOToCar(vo));
+        System.out.println(conversionService.convert(vo, Car.class));
     }
 
 }
